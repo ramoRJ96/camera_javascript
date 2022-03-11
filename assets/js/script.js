6e8995a84f3a4208638961e03ab9e129e2075292
ramoRJ96 <moiseraidjy@gmail.com>
2022-03-11 12:22:10 +0300

    fix bug facingMode

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ user.onclick = () => {
     console.log(constraints.video.facingMode);
     if (constraints.video.facingMode === 'user') constraints.video.facingMode = 'environment';
     else if (constraints.video.facingMode === 'environment') constraints.video.facingMode = 'user';
-    // navigator.mediaDevices.getUserMedia(constraints);
+    startStream(constraints);
     video.play();
 }
 
